861f6587ef94c51f254da6a7cd47453ea36c9a8f
Charlene <c.bennevault@gmail.com>
2020-08-20 17:47:10 +0200

    Create NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import ColorPalette from "./ColorPalette";
 import PaletteList from "./PaletteList";
 import ColorSeed from "./ColorSeed";
-import SingleColorPalette from "./SingleColorPalette"
+import SingleColorPalette from "./SingleColorPalette";
+import NewPaletteForm from "./NewPaletteForm";
 import {Route, Switch} from "react-router-dom";
 import {generatePalette} from "./ColorHelpers";
 
@@ -16,6 +17,7 @@ class App extends Component {
     return (
     <div>
       <Switch>
+        <Route exact path="/palette/new" render={()=> <NewPaletteForm /> }/>
         <Route exact path="/" render={(routeProps) => <PaletteList palette={ColorSeed} {...routeProps} /> } />
         <Route exact path="/palette/:id" 
           render={(routeProps) => <ColorPalette 
